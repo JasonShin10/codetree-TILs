d40b2acd097bee64ba41d449a0f478c89d112d9b
JasonShin <101558745+JasonShin10@users.noreply.github.com>
2023-11-08 14:14:06 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231108/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 231108/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -4,67 +4,49 @@ using namespace std;
 int main() {
     int n, m;
     cin >> n >> m;
-    int rCnt = 1;
-    int cCnt = 1;
-    bool rSame = false;
-    bool cSame = false;
     int ans = 0;
     int arr[100][100];
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             cin >> arr[i][j];
         }
     }
-        if (m == 1) 
-        {
-        cout << 2 * n; 
-        return 0;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        rCnt = 1;
-        for (int j = 1; j < n; j++)
-        {
-        
-                if (arr[i][j - 1] == arr[i][j])
-                {
-                    rCnt++;
-                }
-                else
-                {
-                    if (rCnt >= m)
-                    {
-                        ans++;
-                        break;
-                    }
-                    rCnt = 1;
-                }
-            
-        }
-        if (rCnt >= m) ans++;
-        cCnt = 1;
-        for (int j = 1; j < n; j++)
-        {        
-             if (arr[j - 1][i] == arr[j][i])
-                {
-                    cCnt++;
-                }
-                else 
-                {
-                    if (cCnt >= m)
-                    {
-                        ans++;
-                        break;
-                    }
-                    cCnt = 1;
-                }  
-        }
-        if (cCnt >= m) ans++;
 
+
+    for (int i = 0; i < n; i++) {
+        int rCnt = 1;
+        for (int j = 1; j < n; j++) {
+            if (arr[i][j] == arr[i][j - 1]) {
+                rCnt++;
+            } else {
+                if (rCnt >= m) {
+                    ans++;
+                    break;
+                }
+                rCnt = 1;
+            }
+        }
+        if (rCnt >= m) ans++; 
     }
+
+
+    for (int j = 0; j < n; j++) {
+        int cCnt = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i][j] == arr[i - 1][j]) {
+                cCnt++;
+            } else {
+                if (cCnt >= m) {
+                    ans++;
+                    break;
+                }
+                cCnt = 1;
+            }
+        }
+        if (cCnt >= m) ans++; 
+    }
+
     cout << ans;
     return 0;
 }
