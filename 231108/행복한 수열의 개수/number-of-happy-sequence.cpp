2814e960f4ca7250174c0360810d75b018f544af
JasonShin <101558745+JasonShin10@users.noreply.github.com>
2023-11-08 14:28:45 +0900

    Title: 행복한 수열의 개수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231108/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 231108/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,52 +1,65 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
+#degine MAX_N 100
+
+int n,m;
+int grid[MAX_N][MAX_N];
+
+int seq[MAX_N];
+
+bool IsHappySequence()
+{
+    int consecutive_count =1, max_ccnt =1;
+    for(int i=1; i<n; i++)
+    {
+        if(seq[i - 1] == seq[i])
+        {
+            consecutive_count++;
+        }
+        else{
+            consecutive_count = 1;
+        }
+        max_ccnt = max(max_ccnt, consecutive_count);
+    }
+    return max_ccnt >= m;
+}
 int main() {
-    int n, m;
+    
     cin >> n >> m;
-    int ans = 0;
-    int arr[100][100];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cin >> arr[i][j];
+            cin >> grid[i][j];
         }
     }
-
+    int num_happy =0;
 
     for (int i = 0; i < n; i++) {
-        int rCnt = 1;
-        for (int j = 1; j < n; j++) {
-            if (arr[i][j] == arr[i][j - 1]) {
-                rCnt++;
-            } else {
-                if (rCnt >= m) {
-                    ans++;
-                    break;
-                }
-                rCnt = 1;
-            }
+        for( int j =0; j<n;j++)
+        {
+            seq[j] = grid[i][j];
         }
-        if (rCnt >= m) ans++; 
+        if(IsHappySequence())
+        {
+            num_happy++;
+        }
     }
 
-
-    for (int j = 0; j < n; j++) {
-        int cCnt = 1;
-        for (int i = 1; i < n; i++) {
-            if (arr[i][j] == arr[i - 1][j]) {
-                cCnt++;
-            } else {
-                if (cCnt >= m) {
-                    ans++;
-                    break;
-                }
-                cCnt = 1;
-            }
+    for(int j=0;j<n; j++)
+    {
+        for(int i=0; i<n; i++)
+        {
+        seq[i] = grid[i][j];
         }
-        if (cCnt >= m) ans++; 
-    }
 
-    cout << ans;
+        if(IsHappySequence())
+        {
+            num_happy++;
+        }
+    }
+    cout << num_happy++;
     return 0;
 }
